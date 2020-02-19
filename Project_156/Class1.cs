b2826428d2f1e5166a4b4f192b7c8375c6287a07
Kaley <kaleyrennie14@gmail.com>
2020-02-19 15:49:30 -0800

    Add project files.

---
+++ Project_156/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignment156
+{
+    public class Calculation
+    {
+
+
+        public int Add(int num1)
+        {
+            return num1 + 2;
+        }
+
+        public int Subtract(int num1)
+        {
+            return num1 - 2;
+        }
+
+        public int Divide(int num1)
+        {
+            return num1 / 2;
+        }
+
+
+    }
+
+}
---
+++ Project_156/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace assignment156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+            Calculation calc = new Calculation();
+
+            Console.WriteLine(num1 + " + 2 = ");
+            Console.WriteLine(calc.Add(num1));
+
+            Console.WriteLine(num1 + " - 2 = ");
+            Console.WriteLine(calc.Subtract(num1));
+
+            Console.WriteLine(num1 + " / 2 =");
+            Console.WriteLine(calc.Divide(num1));
+
+
+            Console.Read();
+
+        }
+    }
+}
+
+
+
+ 
